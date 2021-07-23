c93cb83d4ae515a0e8a7ee3458d45985acd23e0d
jpauloleao <jpauloazv@gmail.com>
2021-07-23 11:36:19 -0300

    Login via senha cartão

---
+++ Proposta/src/main/java/br/com/zup/orange/Proposta/Seguranca/SecurityConfiguration.java
@@ -0,0 +1,28 @@
+package br.com.zup.orange.Proposta.Seguranca;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests(authorizeRequests ->
+                authorizeRequests
+                        .antMatchers(HttpMethod.GET, "/proposta/*").hasAuthority("SCOPE_escopo")
+                        .antMatchers(HttpMethod.GET, "/cartao/*").hasAuthority("SCOPE_escopo")
+                        .antMatchers(HttpMethod.POST, "/proposta/").hasAuthority("SCOPE_escopo")
+                        .anyRequest().authenticated()
+        )
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+
+} 
